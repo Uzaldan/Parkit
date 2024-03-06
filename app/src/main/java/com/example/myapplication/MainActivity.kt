535695c8bac4ab67d09ca12af8d673e9539626cb
Uzaldan <zacharyabel101@gmail.com>
2024-03-06 14:23:31 -0800

    Project creation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,17 +18,37 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_mainactivity)
+        val txtview: TextView  = findViewById(R.id.textView);
+        txtview.text= ("PIS");
+        val mapbutton: TextView  = findViewById(R.id.button1);
+        mapbutton.text= ("Map");
+        val lotButton: TextView  = findViewById(R.id.button);
+        lotButton.text= ("Lot List");
+        val favButton: TextView  = findViewById(R.id.button2);
+        favButton.text= ("Favorites");
+        mapbutton.setOnClickListener{
+
+            startActivity(Intent(this, Parking_map::class.java).apply {
+                // you can add values(if any) to pass to the next class or avoid using `.apply`
+
+            })
         }
+        lotButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Hi lots!", Toast.LENGTH_SHORT).show()
+        }
+        favButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Hi favs!", Toast.LENGTH_SHORT).show()
+            favButton.text= ("bean button");
+        }
+        //button3.setOnClickListener{
+//
+  //      }
     }
-}
+    }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
