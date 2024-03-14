14df0984216c0ba8babfa34aeb3229a1343f980a
Uzaldan <zacharyabel101@gmail.com>
2024-03-13 17:51:54 -0700

    added parking.png added circle_button.xml added activity_feedback.xml added activity_parkingmap.xml app barely allows you to click through each piece this is more of a hollow shell to be filled with database info rn.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.cardview.widget.CardView
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,27 +11,49 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainactivity)
         val lotListB = findViewById<Button>(R.id.lotListButton)
         val mapB = findViewById<Button>(R.id.mapButton)
-        val subInfoB = findViewById<Button>(R.id.submitInfoButton)
+        val items = arrayOf("25%", "50%", "75%", "100%")
+        val feedbackB = findViewById<Button>(R.id.feedbackButton)
        // val B= findViewById<Button>(R.id.)
        // val B= findViewById<Button>(R.id.)
         //val B= findViewById<Button>(R.id.)
-       // val B= findViewById<Button>(R.id.)
-       // val B= findViewById<Button>(R.id.)
-        subInfoB.setOnClickListener{
-            setContentView(R.layout.activity_lotlist)
+        //val mapB= findViewById<Button>(R.id.mapButton)
+        var hashMap : HashMap<String, Int> = HashMap<String, Int> ()
+        hashMap.put("" , 3000)
+
+        mapB.setOnClickListener{
+            setContentView(R.layout.activity_parkingmap)
+
         }
+
+      feedbackB.setOnClickListener{
+          setContentView(R.layout.activity_feedback)
+
+      }
         lotListB.setOnClickListener{
             setContentView(R.layout.activity_lotlist)
+
+            val cardkl = findViewById<CardView>(R.id.layoutW)
+            val carda = findViewById<CardView>(R.id.layoutNT)
+            val cardb = findViewById<CardView>(R.id.layoutN)
+            val cardc = findViewById<CardView>(R.id.layoutM)
+            val cardd = findViewById<CardView>(R.id.layoutH)
+            val cardEC = findViewById<CardView>(R.id.layoutEC) .setOnClickListener{
+                    setContentView(R.layout.activity_lotinfo)
+                    val updateB= findViewById<Button>(R.id.LI_Update_button)
+                    updateB.setOnClickListener{
+                        setContentView(R.layout.activity_editlot)
+                    }
+
+                }
         }
-        mapB.setOnClickListener{
-            setContentView(R.layout.activity_lotinfo)
-        }
+        mapB
 
     }
     }
