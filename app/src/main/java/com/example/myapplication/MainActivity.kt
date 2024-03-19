900720e8e6a0686aad8323494585176522c5ce6b
Uzaldan <zacharyabel101@gmail.com>
2024-03-18 19:50:09 -0700

    added parking.png added circle_button.xml added activity_feedback.xml added activity_parkingmap.xml app barely allows you to click through each piece this is more of a hollow shell to be filled with database info rn.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,13 +9,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.Objects
 
+private lateinit var database: DatabaseReference
+
+data class Conditions(
+    var snow: Boolean? = false,
+    var gravel: Boolean? = false,
+    var potholes: Boolean? = false,
+    var ice: Boolean? = false,
+    var plowed: Boolean? = false,
+    var salt: Boolean? = false,
+    var flooded: Boolean? = false,
+    var golfballhail:Boolean? = false,
+
+){
+
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "snow" to snow,
+            "plowed" to plowed,
+            "ice" to ice,
+            "salt" to salt,
+            "flooded" to flooded,
+            "gravel" to gravel,
+            "potholes" to potholes,
+            "golfballhail" to golfballhail,
+        )
+    }
+}
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainactivity)
+        database = Firebase.database.reference
         val lotListB = findViewById<Button>(R.id.lotListButton)
         val mapB = findViewById<Button>(R.id.mapButton)
         val items = arrayOf("25%", "50%", "75%", "100%")
@@ -49,6 +82,7 @@ class MainActivity : ComponentActivity() {
                     val updateB= findViewById<Button>(R.id.LI_Update_button)
                     updateB.setOnClickListener{
                         setContentView(R.layout.activity_editlot)
+
                     }
 
                 }
@@ -73,4 +107,10 @@ fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
+}
+
+fun updateconditions(name: String, array: Array<Boolean>){
+   var dbclot = database.child("conditions").child(name);
+   var setConditions = Conditions(array[0],array[1],array[2],array[3],array[4],array[5],array[6],array[7]);
+   var setConValues = setConditions.toMap();
 }
